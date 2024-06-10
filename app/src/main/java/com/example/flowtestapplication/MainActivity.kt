6656b92de36a933e067734e794b2a076918fc091
Aditya Bhaskar <79363218+ab-gnm@users.noreply.github.com>
2024-06-10 15:53:14 +0100

    Display `TextMeasureSample` in sample activity

--- app/src/main/java/com/example/flowtestapplication/MainActivity.kt
+++ app/src/main/java/com/example/flowtestapplication/MainActivity.kt
@@ -6,11 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.flowtestapplication.nav.ScreenHost
+import com.example.flowtestapplication.textmeasure.TextMeasureSample
 import com.example.flowtestapplication.ui.theme.FlowTestApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,26 +20,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ScreenHost()
-//                    Greeting("Android")
+                    TextMeasureSample()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FlowTestApplicationTheme {
-        Greeting("Android")
     }
 }
